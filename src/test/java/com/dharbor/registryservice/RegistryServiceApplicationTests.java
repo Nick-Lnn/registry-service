401c4ba1567cea334eb3efa7888f179fe9df5866
Nicolas Linn <nicolaslinn.8@gmail.com>
2024-10-23 06:46:51 -0400

    Registry-service

---
+++ src/test/java/com/dharbor/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dharbor.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
